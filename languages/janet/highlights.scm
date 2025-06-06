c36a05ff3e486a00c775ca012aa2248bd339df64
Flajr <mrk.stefanka@gmail.com>
2025-06-06 23:54:17 +0200

    Fixes

--- languages/janet/highlights.scm
+++ languages/janet/highlights.scm
@@ -1,65 +1,81 @@
 ; Comments
 (line_comment) @comment
 
-; Numeric literals
+; Literals
 (number_literal) @number
-
-; String literals
 (str_literal) @string
 (long_str_literal) @string
 (buffer_literal) @string
 (long_buffer_literal) @string
-
-; Boolean and nil constants
 (bool_literal) @constant.builtin
 (nil_literal) @constant.builtin
 
-; Keywords (prefixed with colon)
+; Keywords and symbols
 (keyword) @constant
-
-; Special forms and built-in functions as symbols
-(symbol) @keyword
-(#match? @keyword "^(def|var|varglobal|fn|defn|defmacro|defglobal|do|quote|quasiquote|unquote|splice|if|if-let|when|unless|cond|case|while|for|loop|each|break|continue|return|yield|resume|set|upscope|try|catch|finally|throw|with|with-syms|gensym|import|use|require|module|fiber|thread|ev/spawn|ev/do-thread)$")
-
-; Function calls - first element in tuple
-(tuple
-  . (symbol) @function)
-
-; Macro calls - first element in tuple starting with uppercase
-(tuple
-  . (symbol) @function.macro
-  (#match? @function.macro "^[A-Z]"))
-
-; Built-in arithmetic and comparison operators
-(symbol) @function.builtin
-(#match? @function.builtin "^(\\+|\\-|\\*|\\/|%|=|<|>|<=|>=|not=|and|or|not)$")
-
-; Built-in functions
-(symbol) @function.builtin
-(#match? @function.builtin "^(length|get|put|merge|keys|values|pairs|map|filter|reduce|apply|partial|comp|juxt|first|last|rest|butlast|drop|take|drop-while|take-while|reverse|sort|sort-by|group-by|frequencies|distinct|flatten|partition|partition-by|split-at|split-with|concat|mapcat|interleave|interpose|zip|unzip)$")
-
-; Thread macros
-(symbol) @function.macro
-(#match? @function.macro "^(->|->\\?|->>|->>\\?|as->|some->|some->>|cond->|cond->>)$")
-
-; Default symbols are variables
 (symbol) @variable
 
-; Punctuation
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "@{"
-] @punctuation.bracket
+; Special forms
+(quote_form) @punctuation.special
+(quasiquote_form) @punctuation.special
+(unquote_form) @punctuation.special
+(splice_form) @punctuation.special
+(short_fn) @function
 
-; Special prefixes
-"@" @punctuation.special
+; Quote operators
 "'" @punctuation.special
 "~" @punctuation.special
 "," @punctuation.special
-";~" @punctuation.special
-"~@" @punctuation.special
+";" @punctuation.special
+"quote" @keyword
+"quasiquote" @keyword
+"unquote" @keyword
+"splice" @keyword
+
+; Definition forms
+(def "def" @keyword)
+(def name: (symbol) @function)
+(def metadata: (keyword) @attribute)
+(def metadata: (str_literal) @attribute)
+(var ["var" "varglobal"] @keyword)
+(var name: (symbol) @variable)
+(var metadata: (keyword) @attribute)
+(var metadata: (str_literal) @attribute)
+(function_def ["defn" "defn-" "defglobal" "defmacro" "defmacro-" "defdyn" "fn" "macro"] @keyword)
+(function_def name: (symbol) @function)
+(function_def metadata: (keyword) @attribute)
+(function_def metadata: (str_literal) @attribute)
+
+; Control flow
+(control_flow ["if" "if-let" "if-not" "if-with" "when" "when-let" "when-with" "unless" "unless-let" "cond" "case" "and" "or" "do" "not"] @keyword)
+
+; Loop constructs
+(loop_construct ["while" "for" "loop" "each" "eachk" "eachp" "forever" "repeat"] @keyword)
+
+; Assignment
+(assignment ["set" "setdyn" "put" "put-in" "update" "update-in"] @keyword)
+
+; Special forms
+(special_form ["upscope" "comptime" "try" "error" "errorf" "assert" "assertf" "defer" "edefer" "import" "use" "require" "module" "->" "->>" "-?>" "-?>>"] @keyword)
+
+; Built-in operations
+(operation ["+" "++" "+=" "-" "--" "-=" "*" "*=" "/" "/=" "%" "%=" "inc" "dec" "mod" "div" "abs" "sum" "product" "mean" "max" "min" "=" "not=" "<" "<=" ">" ">=" "compare" "deep=" "deep-not=" "band" "bor" "bxor" "bnot" "blshift" "brshift" "brushift" "array" "get" "get-in" "length" "empty?" "first" "last" "slice" "take" "take-until" "take-while" "drop" "drop-until" "drop-while" "reverse" "reverse!" "sort" "sort-by" "map" "mapcat" "filter" "keep" "reduce" "reduce2" "find" "find-index" "frequencies" "string" "describe" "table" "struct" "keys" "values" "pairs" "merge" "merge-into" "print" "prin" "printf" "eprint" "slurp" "spit" "flush"] @function.builtin)
+
+; Namespace-qualified functions
+((operation) @function.builtin
+  (#match? @function.builtin "^[a-zA-Z][a-zA-Z0-9\\-_]*/[a-zA-Z][a-zA-Z0-9\\-_]*"))
+
+; Function calls
+(generic_tuple head: (symbol) @function)
+
+; Brackets
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"@[" @punctuation.bracket
+"@{" @punctuation.bracket
+
+; Short function delimiters
+"|" @punctuation.special
--- languages/janet/indents.scm
+++ languages/janet/indents.scm
@@ -1,82 +1,16 @@
 ; Basic bracket indentation
-[
-    "("
-    "["
-    "{"
-] @indent
+"(" @indent
+")" @outdent
+"[" @indent
+"]" @outdent
+"{" @indent
+"}" @outdent
 
-; Decrease indentation for closing brackets
-[
-    ")"
-    "]"
-    "}"
-] @outdent
+; Indent contents of data structures
+(array) @indent
+(table) @indent
+(struct) @indent
+(tuple) @indent
 
-; Special forms that should indent their body
-(tuple
-  .
-  (symbol) @_keyword
-  (#match? @_keyword "^(def|defn|defmacro|defglobal|fn|do|let|if|when|unless|cond|case|while|for|loop|each|try|with|with-syms|module)$")
-  (tuple)? @indent
-  .
-  (_) @indent)
-
-; Function definitions with parameter lists
-(tuple
-  .
-  (symbol) @_def
-  (#match? @_def "^(defn|fn)$")
-  .
-  (symbol)
-  .
-  (tuple) @indent.params
-  .
-  (_) @indent)
-
-; Let bindings and similar forms
-(tuple
-  .
-  (symbol) @_let
-  (#match? @_let "^(let|with-dyns|with-vars)$")
-  .
-  (tuple) @indent.bindings
-  .
-  (_) @indent)
-
-; Conditional forms
-(tuple
-  .
-  (symbol) @_cond
-  (#match? @_cond "^(if|when|unless|cond|case)$")
-  .
-  (_) @indent
-  .
-  (_) @indent)
-
-; Loop constructs
-(tuple
-  .
-  (symbol) @_loop
-  (#match? @_loop "^(while|for|loop|each)$")
-  .
-  (_) @indent
-  .
-  (_) @indent)
-
-; Threading macros
-(tuple
-  .
-  (symbol) @_thread
-  (#match? @_thread "^(->|->>|as->|some->|some->>|cond->|cond->>)$")
-  .
-  (_) @indent)
-
-; Data structures
-(array
-  (_) @indent)
-
-(table
-  (_) @indent)
-
-(struct
-  (_) @indent)
+; Additional indentation for multi-line forms
+(short_fn) @indent
